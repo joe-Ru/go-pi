84a643fa55594da37193d848450a8692946966ee
joe-Ru <29042892+joe-Ru@users.noreply.github.com>
2023-11-29 18:31:43 -0500

    added go-chi and the first getAllArticles call
    go gin kinda sucks ngl

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 
+	"encoding/json"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	// "context"
 	// "fmt"
 	// "log"
 	//"encoding/json"
 	"mongo_connection_test/mongo_service"
 	"net/http"
-
 	// "go.mongodb.org/mongo-driver/mongo"
 	// "go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -22,9 +24,18 @@ type Articles struct {
 }
 
 
-func getAllArticles(c *gin.Context) {
+func getAllArticles(w http.ResponseWriter, r *http.Request) {
 	allArticles := mongo_service.GetArticlesFromCollection("medical_articles")
-	c.IndentedJSON(http.StatusOK, allArticles)
+
+	jsonData, err := json.Marshal(allArticles)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error marshaling JSON: " + err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
 
 
@@ -56,11 +67,23 @@ func addArticle(c *gin.Context) {
 
 
 func main() {
-	router := gin.Default()
-	router.GET("/getAllArticles", getAllArticles)
-	router.GET("/getOneArticle:id", getOneArticle)
-	router.GET("/deleteOneArticle:id", deleteOneArticle)
-	router.GET("/addArticle/:title/:link", addArticle)
-	router.Run("localhost:8080")
+	//router := gin.Default()
+	//router.GET("/getAllArticles", getAllArticles)
+	//router.GET("/getOneArticle:id", getOneArticle)
+	//router.GET("/deleteOneArticle:id", deleteOneArticle)
+	//router.GET("/addArticle/:title/:link", addArticle)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/getAllArticles", getAllArticles)
+
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		return 
+	}
 
 }
